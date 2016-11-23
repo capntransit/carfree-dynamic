d9bf5bf3ecc93fc6b0dae654237a97723f0dac33
Cap'n Transit <capntransit@gmail.com>
2016-11-23 04:49:40 +0000

    Added 'In' variable and tract number

--- flask_app.py
+++ flask_app.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 
 CONFIG = {}
 
-API_URL = 'http://api.census.gov/data/2014/acs5?get=NAME,B08141_{var}&for={geo}&key={key}'
+API_URL = 'http://api.census.gov/data/2014/acs5?get=NAME,B08141_{var}&for={forc}&in={inc}&key={key}'
 
-DEFAULT_GEO = 'region:*'
-DEFAULT_VAR = '001E'
+DEFAULT_FOR = 'tract:*'
+DEFAULT_IN = 'state:36 county:81'
 
 LOCAL_DIR = os.path.dirname(__file__)
 
@@ -36,11 +36,12 @@ def census():
         'vars': VARS
         }
 
-    geo = request.values.get('geo', DEFAULT_GEO)
+    forc = request.values.get('for', DEFAULT_FOR)
+    inc = request.values.get('in', DEFAULT_IN)
 
     for var, name in VARS.items():
         res = requests.get(
-            API_URL.format(var=var, geo=geo, key=CONFIG['CENSUS_KEY'])
+            API_URL.format(var=var, inc=inc, forc=forc, key=CONFIG['CENSUS_KEY'])
             ).json()
         res.pop(0)
         for idx, loc in enumerate(res):
@@ -49,13 +50,17 @@ def census():
             if loc[0] not in cols:
                 cols[loc[0]] = {}
             cols[loc[0]][var] = loc[1]
+            cols[loc[0]]['Tract'] = loc[4]
 
     for loc in locs:
-        cols[loc]['Ratio'] = str(
-            round(int(cols[loc]['002E']) / int(cols[loc]['001E']), 3)
-            )
+        cols[loc]['Ratio'] = 0
+        if int(cols[loc]['001E']) > 0:
+            cols[loc]['Ratio'] = str(
+                round(int(cols[loc]['002E']) / int(cols[loc]['001E']), 3)
+                )
 
-    param['geo'] = geo
+    param['in'] = inc
+    param['for'] = forc
     param['cols'] = cols
     return render_template('getdata.html', param=param)
 
